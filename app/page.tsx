5921c0e60bb267eff8f38b656019bc12885a3d53
Hansadhwaja Biswal <hansadhwajabiswal@gmail.com>
2024-06-23 12:40:58 +0530

    update main page

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02] text-white">
+    <main className=" bg-black/[0.96] antialiased bg-grid-white/[0.02] text-white flex flex-col flex-wrap overflow-hidden">
      <HeroSection />
      <FeaturedSection />
      <WhyChooseUs />
